28fd8e8f8622cc85891619b8a9a614bad4366dfe
VaishnaviMahajan <vaishnavikmahajan090308@gmail.com>
2023-12-25 23:15:54 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 console.log("Welcome to the game");
 
 let turn = "X";
+let audioTurn = new Audio("sounds/clicked.mp3");
+
 function changeTurn() {
   if (turn === "X") {
     return "O";
@@ -8,3 +10,22 @@ function changeTurn() {
     return "X";
   }
 }
+
+function checkWin() {}
+
+//Main Game
+
+let boxes = document.getElementsByClassName("box");
+
+Array.from(boxes).forEach((element) => {
+  let boxtext = element.querySelector(".boxtext");
+  element.addEventListener("click", () => {
+    if (boxtext.innerText === "") {
+      boxtext.innerText = turn;
+      turn = changeTurn();
+      checkWin();
+      // audioTurn.play();
+      document.getElementsByClassName("turn")[0].innerText = "Turn for " + turn;
+    }
+  });
+});
